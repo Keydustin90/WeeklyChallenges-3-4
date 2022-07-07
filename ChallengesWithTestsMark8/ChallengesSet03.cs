5c74e5e9ea4dcf1bcacef12b702c4766ab48da4b
Dustin Key <Keydustin90@gmail.com>
2022-07-07 07:03:04 -0500

    Set 3-4

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,47 +7,113 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i])
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
         }
 
-        public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
+        public viod IsSumOfOddsOdd(IEnumerable<int> numbers, bool expected)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0
+            {
+                return false;
+            }
+
+            var sum = numbers.Sum();
+
+            return (sum % 2 != 0);
         }
 
-        public bool PasswordContainsUpperLowerAndNumber(string password)
+        public bool PasswordContainsUpperLowerAndNumber(string password, bool expected)
         {
-            throw new NotImplementedException();
-        }
+            var isLower = false;
+            var isUpper = false;
+            var isNumber = false;
 
-        public char GetFirstLetterOfString(string val)
-        {
-            throw new NotImplementedException();
-        }
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower = true;
+                }
 
-        public char GetLastLetterOfString(string val)
-        {
-            throw new NotImplementedException();
-        }
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper = true;
+                }
 
-        public decimal Divide(decimal dividend, decimal divisor)
-        {
-            throw new NotImplementedException();
-        }
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
 
-        public int LastMinusFirst(int[] nums)
-        {
-            throw new NotImplementedException();
-        }
+                }
 
-        public int[] GetOddsBelow100()
-        {
-            throw new NotImplementedException();
-        }
+                if (isLower == true && isUpper == true && isNumber == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void GetFirstLetterOfString(string val)
+            {
+                return ValueTuple.First();
+            }
+
+            public char GetLastLetterOfString(string val)
+            {
+                return val[val.Length - 1];
+            }
+
+            public void Divide(decimal dividend, decimal divisor, decimal expected)
+            {
+                if (divisor == 0)
+                {
+                    return 0;
+                }
+                return dividend / divisor;
+            }
+
+            public void LastMinusFirst(int[] numbers, int expected)
+            {
+                return nums[nums.Length - 1] - nums[0];
+            }
+
+        public int[] ShouldGetOddsBelow100()
+            {
+                var list = new List<int>();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    if(i % 2 != 0)
+                    {
+                        list.Add(i);
+                    }
+                }
+
+                return list.ToArray();
+            }       
+            
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+                {
+                    words[i] = words[i].ToUpper[]; 
+                }
         }
     }
 }
